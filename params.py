07acf68425d22db916753447263207a43b5aec7d
RynCav <ryancav101@gmail.com>
2025-08-10 16:26:29 -0400

    Update params.py

--- params.py
+++ params.py
@@ -3,12 +3,11 @@ import data as d
 # Hyperparameters
 BATCH_SIZE = 100
 EPOCHS = 10
-lr = 0.0001
-max_lr = 0.015
+lr = 0.001
 dr = 0.7
-DECAY = 1e-6
-PATIENCE = 3
+DECAY = 1e-5
 
 # Dataset used:
 Dataset = d.Data('Datasets/Fashion MNIST/')
 Dataset.normalize()
+
